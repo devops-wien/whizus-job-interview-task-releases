c8fd5824d6212fdf840cac89fa6df6c66582b0ce
devops-wien <devops.wien@gmail.com>
2024-06-03 12:11:45 +0200

    [OPTIMIZE]: update providers.tf - reformat code

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,22 @@
 provider "exoscale" {
-  key     = var.key
-  secret  = var.secret
+  key    = var.key
+  secret = var.secret
 }
+
+# provider "aws" {
+#
+#   endpoints {
+#     s3 = "https://sos-${var.zone}.exo.io"
+#   }
+#
+#   region     = var.zone
+#
+#   # access_key = var.s3_access_key
+#   # secret_key = var.s3_secret_key
+#
+#   # Disable AWS-specific features
+#   skip_credentials_validation = true
+#   skip_region_validation      = true
+#   skip_requesting_account_id  = true
+#   skip_s3_checksum            = true
+# }
