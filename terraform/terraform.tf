16b6095233dde8d92ea648773bda80bc78682a8c
devops-wien <devops.wien@gmail.com>
2024-06-03 11:53:56 +0200

    [OPTIMIZE]: update terraform.tf - update version

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,20 +1,28 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.8.4"
 
   required_providers {
     exoscale = {
       source  = "exoscale/exoscale"
-      version = "0.41.1"
+      version = "0.59.0"
     }
   }
 
   backend "s3" {
-    bucket    = "job-interviews"
+    bucket    = "devops-job-interviews"
     key       = "terraform.tfstate"
-    region    = "at-vie-1"
-    endpoint  = "https://sos-at-vie-1.exo.io"
+    region    = var.zone
+    endpoint  = "https://sos-${var.zone}.exo.io"
+
+    # access_key = var.s3_access_key
+    # secret_key = var.s3_secret_key
+    #
+    # export AWS_ACCESS_KEY_ID=""
+    # export AWS_SECRET_ACCESS_KEY=""
 
     skip_credentials_validation = true
-    skip_region_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
